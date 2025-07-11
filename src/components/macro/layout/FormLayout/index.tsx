c907838cd52456ff39039ab629a0ff73ce52a934
FrancoGaytanEndava <franco.gaytan@endava.com>
2025-07-11 16:16:36 -0300

    onclick deleted

--- src/components/macro/layout/FormLayout/index.tsx
+++ src/components/macro/layout/FormLayout/index.tsx
@@ -1,6 +1,5 @@
 import { JSX, PropsWithChildren } from 'react';
 import styles from './styles.module.scss';
-import router from 'next/router';
 import LinkCustom from '@components/micro/LinkCustom';
 
 interface FormLayoutProps {
@@ -12,14 +11,7 @@ export default function FormLayout(props: PropsWithChildren<FormLayoutProps>): J
     <div className={styles.formLayout}>
       <form onSubmit={e => props.onSubmit?.(e)}>
         <div className={styles.containerLayout}>
-          <LinkCustom
-            href="/"
-            className={styles.closeBtn}
-            onClick={() => {
-              router.push('/');
-              router.reload();
-            }}
-            aria-label="Close"></LinkCustom>
+          <LinkCustom href="/" className={styles.closeBtn} aria-label="Close"></LinkCustom>
           {props.children}
         </div>
       </form>
