63f46b1c17b63cae26cef875ab5933cb5be53a15
FrancoGaytanEndava <franco.gaytan@endava.com>
2025-07-17 16:21:25 -0300

    building soliving problem

--- src/app/[lang]/login/page.tsx
+++ src/app/[lang]/login/page.tsx
@@ -1,8 +1,8 @@
 import LoginForm from '@components/pages/LoginForm';
 import { getTranslationServer } from '@hooks/useTranslationServer';
 
-export default async function LoginPage(props: any) {
-  const lang = props?.params?.lang ?? 'es-AR';
+export default async function LoginPage(props: { params: { lang: string } }) {
+  const lang = props.params.lang;
   const t = await getTranslationServer(lang, 'login');
 
   return <LoginForm t={t} lang={lang} />;
