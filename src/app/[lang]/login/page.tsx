d57e1960c32a1aeafaacc5f2ff2db88e29dc5d46
FrancoGaytanEndava <franco.gaytan@endava.com>
2025-07-17 16:28:24 -0300

    f

--- src/app/[lang]/login/page.tsx
+++ src/app/[lang]/login/page.tsx
@@ -1,8 +1,8 @@
 import LoginForm from '@components/pages/LoginForm';
 import { getTranslationServer } from '@hooks/useTranslationServer';
 
-export default async function LoginPage(props: { params: { lang: string } }) {
-  const lang = await props.params.lang;
+export default async function LoginPage(props: { params: Promise<{ lang: string }> }) {
+  const { lang } = await props.params;
   const t = await getTranslationServer(lang, 'login');
 
   return <LoginForm t={t} lang={lang} />;
