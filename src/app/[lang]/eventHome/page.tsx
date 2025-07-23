82d068abedc121b7e154eedb1114e3189fe73ff6
FrancoGaytanEndava <franco.gaytan@endava.com>
2025-07-23 11:46:10 -0300

    eventHome component created

--- src/app/[lang]/eventHome/page.tsx
+++ src/app/[lang]/eventHome/page.tsx
@@ -1,10 +1,12 @@
 import PrivateFooter from '@components/macro/layout/PrivateFooter';
 import PrivateHeader from '@components/macro/layout/PrivateHeader';
+import EventHome from '@components/pages/EventHome';
 
 export default async function EventHomePage() {
   return (
     <>
       <PrivateHeader />
+      <EventHome />
       <PrivateFooter />
     </>
   );
